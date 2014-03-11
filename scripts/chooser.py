fef5c3844ce4676d1644eb397782c295034b2b73
plaix <sotayamy@yahoo.co.uk>
2014-03-11 18:01:33 +0100

    fuli changes

--- scripts/chooser.py
+++ scripts/chooser.py
@@ -4,26 +4,29 @@ import os
 import random
 
 DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data/')
-options = {0: "", 1: "A", 2: "B", 3: "C", 4: "D"}
 
-with open(DATA_PATH + 'quiz_test.dat', 'r') as f:
-    quests = f.readlines()
-    f.close()
 
-quiz_list = []
+class Quiz:
+    options = {0: "", 1: "A", 2: "B", 3: "C", 4: "D"}
 
-for line in quests:
-    quiz_list.append(line.split(','))
+    def __init__(self, path='./'):
+        try:
+            with open(path + 'quiz_test.dat', 'r') as quiz_file:
+                quests = quiz_file.readlines()
+                self.quiz_list = [line.split(',') for line in quests]
+        except IOError:
+            print "Cannot find quiz_test.dat in the given path!"
 
-random.shuffle(quiz_list)
+    def check(self):
+        test = self.quiz_list[:]
+        random.shuffle(test)
 
-for l in quiz_list:
-    answer = str(l[len(l)-1]).rstrip("\n").strip()
-    ll = l[:len(l)-1]
-    for k, a in enumerate(ll):
-        print options[k], a
-    user_answer = raw_input("Enter your answer: ")
-    if answer == user_answer:
-        print "Correct answer"
-    else:
-        print "Wrong answer"
+        for question in test:
+            answer = str(question[-1]).strip()
+            for k, a in enumerate(question[:-1]):
+                print Quiz.options[k], a
+            user_answer = raw_input("Enter your answer: ")
+            if answer == user_answer:
+                print "Correct answer"
+            else:
+                print "Wrong answer"
