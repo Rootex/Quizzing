1321bb4083d42f6e95591c2653217792f010cfda
plaix <sotayamy@yahoo.co.uk>
2014-03-11 01:28:51 +0100

    compare bug

--- scripts/chooser.py
+++ scripts/chooser.py
@@ -4,6 +4,7 @@ import os
 import random
 
 DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data/')
+options = {0: "", 1: "A", 2: "B", 3: "C", 4: "D"}
 
 with open(DATA_PATH + 'quiz_test.dat', 'r') as f:
     quests = f.readlines()
@@ -17,7 +18,12 @@ for line in quests:
 random.shuffle(quiz_list)
 
 for l in quiz_list:
-    answer = l[len(l)-1].replace('\n', '')
-    for k, a in enumerate(l):
-        print k, a
-    break
+    answer = str(l[len(l)-1]).rstrip("\n").strip()
+    ll = l[:len(l)-1]
+    for k, a in enumerate(ll):
+        print options[k], a
+    user_answer = raw_input("Enter your answer: ")
+    if answer == user_answer:
+        print "Correct answer"
+    else:
+        print "Wrong answer"
