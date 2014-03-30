dc3a518c8111adcf3dc1d7e66a01ddd82ae06a76
plaix <sotayamy@yahoo.co.uk>
2014-03-30 23:57:16 +0200

    text2speech

---
+++ scripts/text2speech.py
@@ -0,0 +1,6 @@
+__author__ = 'Sotaya'
+
+'''
+This module will enable us to provide reading the questions and options in audio
+incase the users do not want to read, they can listen instead and provide the answer.
+'''
