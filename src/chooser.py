85aa75a82df4b973b178f89da2f8b6ad72168903
Sotaya <sotayamy@gmail.com>
2016-02-18 15:08:46 +0100

    Debugging

--- src/chooser.py
+++ src/chooser.py
@@ -2,7 +2,7 @@ __author__ = 'Sotaya'
 
 import os
 import random
-import text2speech
+import tts
 import json
 
 DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data/')
@@ -13,13 +13,13 @@ class MutiChoice:
     stats = {"questions_answered": 0, "correct_answer": 0}
 
     def __init__(self, path='./'):
-        self.talk = text2speech.Reader()
+        self.talk = tts.Reader()
         try:
             with open(path + 'quiz_test_multi_choice.dat', 'r') as quiz_file:
                 quests = quiz_file.readlines()
                 self.quiz_list = [line.split(',') for line in quests]
         except IOError:
-            print "Cannot find quiz_test_multi_choice.dat in the given path!"
+            print("Cannot find quiz_test_multi_choice.dat in the given path!")
 
     def check(self):
         test = self.quiz_list[:]
@@ -28,19 +28,19 @@ class MutiChoice:
         for question in test:
             answer = str(question[-1]).strip()
             for k, a in enumerate(question[:-1]):
-                print MutiChoice.options[k], a
+                print(MutiChoice.options[k], a)
                 self.talk.speak(MutiChoice.options[k])
                 self.talk.speak(a)
-            user_answer = raw_input("Enter your answer: ")
+            user_answer = input("Enter your answer: ")
             if answer == user_answer:
-                print "Correct answer"
+                print("Correct answer")
                 self.talk.speak("Correct answer")
                 self.stats["questions_answered"] += 1
                 self.stats["correct_answer"] += 1
             elif user_answer == "":
                 pass
             else:
-                print "Wrong answer"
+                print("Wrong answer")
                 self.talk.speak("Wrong answer")
                 self.stats["questions_answered"] += 1
         with open(DATA_PATH+'stats.json', 'a') as f:
@@ -49,7 +49,7 @@ class MutiChoice:
 
 class Essay:
     def __init__(self, path='./'):
-        self.talk = text2speech.Reader()
+        self.talk = tts.Reader()
         try:
             with open(path + 'quiz_essay.dat', 'r') as quiz_file:
                 quests = quiz_file.read()
@@ -58,22 +58,22 @@ class Essay:
                 for i in self.seg:
                     self.quiz_list[i.split(',')[0]] = i.split(',')[1]
         except IOError:
-            print "Cannot find quiz_test_multi_choice.dat in the given path!"
+            print("Cannot find quiz_test_multi_choice.dat in the given path!")
 
     def check(self):
         test = self.quiz_list
         try:
             for key, question in enumerate(test):
-                print question + '\n'
-                response = raw_input("To show answer enter (show) or (skip) to continue: ")
+                print(question + '\n')
+                response = input("To show answer enter (show) or (skip) to continue: ")
                 if response.lower() == "show":
-                    print test[question] + '\n'
+                    print(test[question] + '\n')
                 elif response.lower() == "skip":
                     pass
                 else:
                     raise ValueError("Wrong response")
         except ValueError as e:
-            print e.message
+            print(e.message)
             raise ValueError()
 
 if __name__ == '__main__':
